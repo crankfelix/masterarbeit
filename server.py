f15d92f83849963d8537c3d7015e5866da4f38f2
Felix Kath <crank_felix@gmx.de>
2018-03-14 00:25:22 +0100

    added zip handling

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, json, request, abort, make_response
 import base64
 import uuid
+import os
 
 app = Flask(__name__)
 
@@ -10,16 +11,28 @@ def index():
 
 @app.route("/api/v1/data",methods=["GET"])
 def return_data():
-	with open("C:\\Users\\Felix\\Pictures\\IMG_2696.JPG","rb") as picture:
+	cwd = os.getcwd()
+	returnJson = dict()
+
+	for file in os.listdir(cwd+"\\pictures"):
+		filename = os.fsdecode(file)
+
+		with open(cwd+"\\pictures\\"+file, "rb") as zipFile:
+			retValue = str(base64.b64encode(zipFile.read()))
 		
-		retValue = base64.b64encode(picture.read())
-		return retValue
+		returnJson[filename] = retValue
+		os.remove(cwd+"\\pictures\\"+file)
+
+	return json.dumps(returnJson)
+
+
 
 @app.route("/api/v1/model",methods=["POST"])
 def accept_model():
 	if not request.json:
 		abort(400)
-	with open(str(uuid.uuid4())+".json","w+") as file:
+	cwd = os.getcwd()
+	with open(cwd+"\\models\\"+str(uuid.uuid4())+".json","w+") as file:
 		file.write(str(request.json))
 
 	return "nice",201
@@ -27,3 +40,7 @@ def accept_model():
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
+#start local testing with pictures
+#get results back to check which model is the best by using random pictures 
+#delete bad models
